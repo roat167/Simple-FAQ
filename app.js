202a2c9a90a161dfe21da48342c0ba78802faa70
khemroat <khemroat@gmail.com>
2017-08-21 16:32:51 -0700

    [Updated]: changes on package json and app js

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 const express = require('express');
-const http = require('http');
-const path = require('path');
-
-const api = require('./server/routes/api');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(__dirname + '/src/assets'));
 
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'dist/index.html'));
+	res.sendFile(__dirname + '/src/index.html');
 });
 
 const port = process.env.PORT || '4200';
 app.set('port', port);
 
-const server = http.createServer(app);
-server.listen(port, () => console.log('Running')); 
+server.listen(port, () => console.log('App is running on port', app.get('port')); 
